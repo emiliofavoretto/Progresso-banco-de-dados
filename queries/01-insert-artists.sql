3d93b44f7396039e7e06be6761e2071986e7c93b
emilio <favorettoemilio@gmail.com>
2025-09-15 16:01:17 -0300

    cria as queries de select

--- queries/01-insert-artists.sql
+++ queries/01-insert-artists.sql
@@ -7,6 +7,13 @@ CREATE TABLE artistas (
     genero_musical VARCHAR (30),
     ano_lancamento INTEGER,
     pais_de_origem VARCHAR (50),
+    musica_famosa VARCHAR (50),
     ativo BOOLEAN
 );
 
+INSERT INTO artistas (nome, tipo, membros, genero_musical, ano_de_lancamento, pais_de_origem, musica_famosa, ativo):
+VALUES
+("Adele", "solo", 1, "pop", 2006, "reino unido", "rolling in the deep", TRUE),
+("21 pilotos", "banda", 2, "rock altenativo", 2009, "estados unidos", "stressed out", TRUE)
+
+console.log("hello world")
---
+++ queries/02-select.sql
@@ -0,0 +1,12 @@
+SELECT * FROM artistas;
+
+SELECT COUNT(*) AS total_artistas FROM artistas;
+
+SELETC nome, genero_musical FROM artistas;
+
+SELETC nome, genero_musical FROM artistas;
+WHERE  pais_de_origem = "Braisl";
+
+SELECT nome, genero_musical FROM artistas;
+WHERE  id = 3 AND id = 4;
+
