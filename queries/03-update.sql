3eca658bff71de618719b2197740096445b09fb7
emilio <favorettoemilio@gmail.com>
2025-09-22 09:01:58 -0300

    comandos iniciais de update

---
+++ queries/03-update.sql
@@ -0,0 +1,11 @@
+UPDATE usuarios SET username = 'PorGAMER2025'
+WHERE username = 'NoobMaster';
+
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET pontos = pontos + 1000;
+
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET email = 'master@gmail.com'
+WHERE email = 'master@email.com';
